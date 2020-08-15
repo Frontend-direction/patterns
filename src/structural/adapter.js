3755d3929dbf10020c2cc0e514d4c4146d927a51
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-15 15:54:13 +0300

    added adapter pattern

---
+++ src/structural/adapter.js
@@ -0,0 +1,36 @@
+class OldCalc {
+  operations(num1, num2, operand) {
+    switch(operand) {
+      case 'add': return num1 + num2;
+      case 'sub': return num1 - num2;
+      default: return NaN;
+    }
+  }
+}
+
+class newCalc {
+  add(num1, num2) {
+    return num1 + num2;
+  }
+
+  subtract(num1, num2) {
+    return num1 - num2;
+  }
+}
+
+class calcAdapter {
+  constructor() {
+    this.calc = new newCalc();
+  }
+
+  operations(num1, num2, operand) {
+    switch(operand) {
+      case 'add': return this.calc.add(num1, num2);
+      case 'sub': return this.calc.add(num1, num2);
+      default: return NaN;
+    }
+  }
+}
+
+const adaptCalc = new calcAdapter();
+const result = adaptCalc.operations(1, 3, 'add'); // Use newCalc but with oldCal interface
