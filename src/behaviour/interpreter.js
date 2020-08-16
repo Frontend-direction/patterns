14a1b82ebd6b397dac598f53be3e9c1d44503156
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-16 20:38:17 +0300

    added interpreter pattern

---
+++ src/behaviour/interpreter.js
@@ -0,0 +1,35 @@
+class Sum {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  interpret() {
+    return this.left.interpret() + this.right.interpret();
+  }
+}
+
+class Min {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  interpret() {
+    return this.left.interpret() - this.right.interpret();
+  }
+}
+
+class Num {
+  constructor(val) {
+    this.val = val;
+  }
+
+  interpret() {
+    return this.val;
+  }
+}
+
+
+const result = new Sum(new Num(100), new Min(new Num(100), new Num(50)));
+result.interpret();
