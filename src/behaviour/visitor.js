c6ceb3c3e888533f3dedbf0afec202d24a83019c
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-16 20:19:37 +0300

    added visitor pattern

---
+++ src/behaviour/visitor.js
@@ -0,0 +1,27 @@
+class Ninja {
+  constructor(name, power) {
+    this.name = name
+    this.power = power
+  }
+
+  getPowerLevel () {
+    return this.power
+  }
+  
+  setPowerLevel(power) {
+    this.power = power;
+  };
+
+  
+  acceptVisitor(functionVisitor) {
+    return functionVisitor(this);
+  } 
+}
+
+const splinter = new Ninja("Splinter", 100);
+const visitor = (ninja) => ninja.setPowerLevel(ninja.getPowerLevel() * 100);
+
+
+splinter.acceptVisitor(visitor);
+
+console.log(splinter.getPowerLevel());
