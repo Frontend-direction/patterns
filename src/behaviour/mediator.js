f57501ca914624b8c66614b2fa2e9a5dc5204b7a
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-16 14:17:30 +0300

    added mediator pattern

---
+++ src/behaviour/mediator.js
@@ -0,0 +1,27 @@
+class TrafficTower {
+  constructor() {
+    this.airplanes = [];
+  }
+
+  requestPositions() {
+    return this.airplanes.map(airplane => {
+      return airplane.position;
+    });
+  }
+}
+
+class Airplane {
+  constructor(position, trafficTower) {
+    this.position = position;
+    this.trafficTower = trafficTower;
+    this.trafficTower.airplanes.push(this);
+  }
+
+  requestPositions() {
+    return this.trafficTower.requestPositions();
+  }
+}
+
+const tower = new TrafficTower();
+const an70 =  new Airplane(10, tower);
+const boing =  new Airplane(15, tower);
