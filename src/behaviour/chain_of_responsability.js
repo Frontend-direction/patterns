3fa7526e7c0170f119102cc607e059e91af4d85c
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-16 09:55:07 +0300

    added chainOfResponsability pattern

---
+++ src/behaviour/chain_of_responsability.js
@@ -0,0 +1,80 @@
+// First example
+export class mySpends {
+  constructor(initialVal = 0) {
+    this.initialVal=initialVal;
+  }
+
+  add(val) {
+    this.initialVal += val;
+    return this;
+  }
+}
+
+const spends = new mySpends();
+console.log(spends.add(20).add(50).initialVal)
+
+// Second Example
+class ShoppingCart {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(p) {
+    this.products.push(p);
+  };
+}
+
+class Discount {
+  calc(products) {
+    let ndiscount = new NumberDiscount();
+    let pdiscount = new PriceDiscount();
+    let none = new NoneDiscount();
+    ndiscount.setNext(pdiscount);
+    pdiscount.setNext(none);
+    return ndiscount.exec(products);
+  };
+}
+
+class NumberDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  };
+
+  exec(products) {
+    let result = 0;
+    if (products.length > 3)
+      result = 0.05;
+
+    return result + this.next.exec(products);
+  };
+}
+
+class PriceDiscount {
+  constructor() {
+    this.next = null;
+  }
+
+  setNext(fn) {
+    this.next = fn;
+  };
+
+  exec(products) {
+    let result = 0;
+    let total = products.reduce((a, b) => a + b);
+
+    if (total >= 500)
+      result = 0.1;
+
+    return result + this.next.exec(products);
+  };
+}
+
+class NoneDiscount {
+  exec() {
+    return 0;
+  };
+}
