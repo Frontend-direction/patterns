1fdd36ac0a785dc47a902da8273fc17d2dd34fb9
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-08-16 13:45:36 +0300

    added iterator pattern

---
+++ src/behaviour/iterator.js
@@ -0,0 +1,69 @@
+// Option 1
+class Iterator {
+  constructor(items) {
+    this.items = items;
+    this.index = 0
+  }
+
+  hasNext() {
+    return this.index < this.items.length; 
+  }
+
+  next() {
+    return this.items[this.index++];
+  }
+}
+
+const iter = new Iterator([1,2,3]);
+for(let i = 0; iter.hasNext(); i++) {
+  console.log(iter.next());
+}
+
+// Options 2
+class MyIterator {
+  constructor(data) {
+    this.index = 0
+    this.data = data
+  }
+
+  [Symbol.iterator]() {
+    return {
+      next: () => {
+        if (this.index < this.data.length) {
+          return {
+            value: this.data[this.index++],
+            done: false
+          }
+        } else {
+          this.index = 0
+          return {
+            done: true,
+            value: void 0
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// Options 3
+function* generator(collection) {
+  let index = 0
+
+  while (index < collection.length) {
+    yield collection[index++]
+  }
+}
+
+
+const iterator = new MyIterator(['This', 'is', 'iterator'])
+const gen = generator(['This', 'is', 'iterator'])
+
+// for (const val of gen) {
+//   console.log('Value: ', val)
+// }
+
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
