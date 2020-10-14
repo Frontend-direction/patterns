a1a7595e58832b655dd4dbf03978686813839758
vovapankiw <vovapankiw@gmail.com>
2020-10-14 10:42:31 +0300

    added test to factory pattern

--- test/factory.spec.js
+++ test/factory.spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-import { WindowsConfig, LinuxConfig, MacConfig, OsFactory } from '../src/creational/factory';
+import { OsFactory } from '../src/creational/factory';
 
 describe('Factory pattern', () => {
   it('should create Linux config instance', () => {
@@ -10,5 +10,15 @@ describe('Factory pattern', () => {
   it('should Linux spec symbol is properly set', () => {
     const linuxConfig = OsFactory.create('dell G5', 'linux');
     expect(linuxConfig.specificSymbols).to.equal('$');
+  });
+
+  it('should create Windows config instance', () => {
+    const windowsConfig = OsFactory.create('dell G5', 'windows');
+    expect(windowsConfig.os).to.equal('Windows');
+  });
+
+  it('should create MacOS config instance', () => {
+    const macConfig = OsFactory.create('dell G5', 'mac');
+    expect(macConfig.os).to.equal('MacOS');
   });
 });
