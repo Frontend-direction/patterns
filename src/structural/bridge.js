77a193452a63deaea9849c46c4c4896ab4b68a79
vovapankiw <vovapankiw@gmail.com>
2020-10-24 19:41:09 +0300

    added one more example to bridge pattern

--- src/structural/bridge.js
+++ src/structural/bridge.js
@@ -1,3 +1,44 @@
+
+// Option 1
+class BuildCompany {
+  constructor(wallCreator) {
+    this._wallCreator = wallCreator;
+  }
+
+  set wallCreator(creator) {
+    this._wallCreator=creator;
+  }
+
+  buildFoundation() {
+    console.log(`_____________`);
+  }
+
+  buildRoom() {
+    this.wallCreator.buildWallWithDoor();
+    this.wallCreator.buildWallWithWindow();
+    this.wallCreator.buildWall();
+    this.wallCreator.buildWall();
+  }
+
+  buildRoof() {
+    console.log(`^^^^^^^^^^^^^
+                    ^^^^^^^`);
+  }
+}
+
+class BrickWallCreator {
+  buildWallWithDoor() {
+    console.log('|-|')
+  }
+  buildWallWithWindow() {
+    console.log('|{}|')
+  }
+  buildWall() {
+    console.log('||')
+  }
+}
+
+// Option 2
 export class BookReader {
   constructor(screen) {
     this.screen = screen;
